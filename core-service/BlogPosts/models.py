3d35182c8293a510554bbea834c121a9b16ad9f7
Alaa <alaalsq1@gmail.com>
2024-09-21 12:06:00 +0100

    create BlogPost model

--- core-service/BlogPosts/models.py
+++ core-service/BlogPosts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255,blank=False)
+    content = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at'] 
