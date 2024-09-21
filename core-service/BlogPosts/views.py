a35345ef19ca8914de150447804b32cf35b36782
Alaa <alaalsq1@gmail.com>
2024-09-21 13:01:14 +0100

    added django-filter package for search functionality and applied search func on the cache data

--- core-service/BlogPosts/views.py
+++ core-service/BlogPosts/views.py
@@ -1,29 +1,33 @@
-from rest_framework import viewsets, permissions
-from .models import BlogPost
-from .serializers import BlogPostSerializer,BlogPostCreateUpdateSerializer
-from django.core.cache import cache
+from rest_framework import viewsets, permissions, filters
 from rest_framework.response import Response
+from .models import BlogPost
+from .serializers import BlogPostSerializer, BlogPostCreateUpdateSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from django.core.cache import cache
 from .pagination import BlogPostPagination
 
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title', 'content']
     pagination_class = BlogPostPagination
-    
+
     def list(self, request, *args, **kwargs):
         cache_key = f'blog_posts_{request.query_params.urlencode()}'
         posts = cache.get(cache_key)
 
         if not posts:
-            page = self.paginate_queryset(self.get_queryset())
+            queryset = self.filter_queryset(self.get_queryset())
+            page = self.paginate_queryset(queryset)
             if page is not None:
                 serializer = self.get_serializer(page, many=True)
                 posts = serializer.data
                 cache.set(cache_key, posts, 60 * 15)
                 return self.get_paginated_response(posts)
-            
-        return Response(posts)    
+
+        return Response(posts)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -31,5 +35,4 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action in ['create', 'update']:
             return BlogPostCreateUpdateSerializer
-
-        return BlogPostSerializer 
+        return BlogPostSerializer
--- core-service/core/settings.py
+++ core-service/core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'Users',
-    'BlogPosts'
+    'BlogPosts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
