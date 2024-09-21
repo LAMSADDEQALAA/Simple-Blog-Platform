d3a6d5f17d13275de441bc1e5c07994a7ad7d0f2
Alaa <alaalsq1@gmail.com>
2024-09-21 11:49:32 +0100

    added generic view with serializer for user registration

--- core-service/Users/views.py
+++ core-service/Users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import UserCreateUpdateSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class UserRegistrationView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateUpdateSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response({"id": response.data["id"], "username": response.data["username"]}, status=response.status_code)
